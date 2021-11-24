3c9d0754812755b97f3ff0f0561be977992d84d6
themichaelreimer <themichaelreimer@gmail.com>
2021-11-23 22:16:39 -0600

    Camera

--- src/entities/camera.lua
+++ src/entities/camera.lua
@@ -1,6 +1,7 @@
 local Camera = class('Camera')
 
-Camera.speedMult = 100.0
+Camera.speedMult = 10.0
+Camera.snapDist = 0.0
 
 function Camera:init(x, y)
 	self.body = love.physics.newBody(physicsWorld, x, y, "dynamic")
@@ -9,7 +10,12 @@ end
 function Camera:update(dt)
 	if not player then return end
 	local r = getVectorBetweenBodies(self.body, player.collider.body)
-	self.body:setLinearVelocity(r.x * self.speedMult, r.y * self.speedMult)
+	if r.r < self.snapDist then
+		self.body:setPosition(player.collider.body:getPosition())
+		self.body:setLinearVelocity(0, 0)
+	else
+		self.body:setLinearVelocity(r.x * self.speedMult, r.y * self.speedMult)
+	end
 end
 
 return Camera
--- src/helpers.lua
+++ src/helpers.lua
@@ -1,12 +1,22 @@
-function getVectorBetweenBodies(b1, b2)
+function getVectorBetweenBodies(b1, b2, normalize)
+	local shouldNormalize = normalize or false
 	local p1x, p1y = b1:getPosition()
 	local p2x, p2y = b2:getPosition()
 	local dx = p2x - p1x
 	local dy = p2y - p1y
 
 	local r = math.sqrt(dx * dx + dy * dy)
-	return {
-		x = dx / r,
-		y = dy / r
-	}
+	if shouldNormalize then
+		return {
+			x = dx / r,
+			y = dy / r,
+			r = r
+		}
+	else
+		return {
+			x = dx,
+			y = dy,
+			r = r
+		}
+	end
 end
